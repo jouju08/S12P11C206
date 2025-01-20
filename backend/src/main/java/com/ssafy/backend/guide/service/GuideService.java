eeba3f0df59b53468f6845d154b79b3297bd19c1
byeongju <bottleaprk28@gmail.com>
2025-01-20 11:58:44 +0900

    [BE] feat: Redis 연결 설정 및 가이드 코드 작성 (#106)

--- backend/src/main/java/com/ssafy/backend/guide/service/GuideService.java
+++ backend/src/main/java/com/ssafy/backend/guide/service/GuideService.java
@@ -1,6 +1,10 @@
 package com.ssafy.backend.guide.service;
 
 import com.ssafy.backend.common.exception.BadRequestException;
+import com.ssafy.backend.guide.model.Guide;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 /*
@@ -12,12 +16,32 @@ import org.springframework.stereotype.Service;
  * */
 
 @Service
+@RequiredArgsConstructor
 public class GuideService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
     public void tryCatchGuide() {
         try{
             // Service logic
         }catch(Exception e){
             throw new BadRequestException("잘못된 요청입니다.");
         }
+    }
+
+    public void redisInOutGuide() {
+        Guide guide = new Guide(2L, "guide", 3000);
+        // 1. 값 넣기
+        ValueOperations<String, Object> ops = redisTemplate.opsForValue();
+        ops.set("guide", guide);
+
+        // 값 조회
+        ValueOperations<String, Object> ops2 = redisTemplate.opsForValue();
+        Guide output = (Guide) ops2.get("guide");
+        System.out.println(output);
+
+        // 값 삭제
+        redisTemplate.delete("guide");
+        Guide output2 = (Guide) ops2.get("guide");
+        System.out.println(output2); // -> null
     }
 }
