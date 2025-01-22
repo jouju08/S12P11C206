72807bae866cef1ca27b9e578910ada256a75c47
chaehyeon <chi_927@naver.com>
2025-01-22 17:43:26 +0900

    [FE] feat: login 후 header 만들기

--- frontend/src/common/layout/MainLayout.jsx
+++ frontend/src/common/layout/MainLayout.jsx
@@ -7,9 +7,9 @@ import DefaultHeader from '../Header/DefaultHeader';
 export default function MainLayout() {
   const { isAuthenticated } = useAuth();
   return (
-    <div>
+    <div className="bg-main-background">
       {isAuthenticated ? <AuthHeader /> : <DefaultHeader />}
-      <div className="flex w-3/4 min-h-screen bg-slate-400 justify-center items-center mx-auto">
+      <div className="flex w-3/4 min-h-screen justify-center items-center mx-auto border-solid border-2 border-indigo-600">
         <Outlet />
       </div>
       <Footer />
