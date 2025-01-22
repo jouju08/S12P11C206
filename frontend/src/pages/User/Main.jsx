1d613e76485838006a3ccbf6c3c3de6f0e9b561f
chaehyeon <chi_927@naver.com>
2025-01-22 17:44:18 +0900

    [FE] feat: 만든 NavMenu 컴포넌트 확인

--- frontend/src/pages/User/Main.jsx
+++ frontend/src/pages/User/Main.jsx
@@ -1,5 +1,40 @@
 import React from 'react';
+import NavMenu from '@/components/Main/NavMenu';
 
 export default function Main() {
-  return <div>Main</div>;
+  const imgArray = [
+    'nav-colored-pencils.png',
+    'nav-book.png',
+    'nav-drawing.png',
+    'nav-proud.png',
+  ];
+  const menuArray = [
+    <>
+      동화 <br /> 만들기
+    </>,
+    <>
+      내 동화 <br /> 책장
+    </>,
+    <>
+      내 그림 <br /> 꾸러미
+    </>,
+    <>
+      그림 <br /> 구경
+    </>,
+  ];
+
+  const listNavMenu = imgArray.map((image, idx) => (
+    <li key={idx}>
+      <NavMenu location={image}>{menuArray[idx]}</NavMenu>
+    </li>
+  ));
+
+  console.log(listNavMenu);
+  return (
+    <div>
+      <ul className="h-[200px] justify-start items-center gap-5 flex overflow-hidden">
+        {listNavMenu}
+      </ul>
+    </div>
+  );
 }
