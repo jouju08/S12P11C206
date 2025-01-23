3ec0d209d1fcde7e26d06f3b313b8f228ec0c133
byeongju <bottleaprk28@gmail.com>
2025-01-23 17:45:04 +0900

    [BE] refactor: 로그인 공통 객체, 타입 정리

--- backend/src/main/java/com/ssafy/backend/member/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/backend/member/controller/AuthController.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.val;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,32 +28,21 @@ public class AuthController {
     //회원가입
     @PostMapping("/register")   //말 그대로 회원 가입
     public ApiResponse<Object> register(
-            @Valid @RequestBody Member member){
+            @Valid @RequestBody Member member) {
         authenticationService.register(member);
         return ApiResponse.builder().data("성공").build();
     }
 
     @PostMapping("/login/jwt")
     @ResponseBody
-    public Map<String, String> loginJwt(@RequestBody Map<String, String> data,
-                                        HttpServletResponse response) {
-        System.out.println("authToken");
-        val authToken=new UsernamePasswordAuthenticationToken(data.get("memberId"), data.get("password"));
-        System.out.println(authToken);
-        val auth=authenticationManagerBuilder.getObject().authenticate(authToken);
-        System.out.println("authToken2");
+    public ApiResponse<Map<String, String>> loginJwt(@RequestBody Map<String, String> data) {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(data.get("loginId"), data.get("password"));
+        Authentication auth = authenticationManagerBuilder.getObject().authenticate(authToken);
         SecurityContextHolder.getContext().setAuthentication(auth);
-        System.out.println("authToken3");
-        val jwt= JwtUtil.createToken(SecurityContextHolder.getContext().getAuthentication());//함수 계속 재사용하는거라 단순 계산에만 사용
-        var cookie=new Cookie("jwt",jwt);
-        System.out.println("authToken4");
-        cookie.setMaxAge(10);//쿠키 수명
-        cookie.setHttpOnly(true);//쿠키 가로채기 금지
-        cookie.setPath("/");//쿠키가 전송될 경로
-        response.addCookie(cookie);
-        Map<String,String> map=new HashMap<>();
-        map.put("token",jwt);
-        return map;
+        String jwt = JwtUtil.createToken(SecurityContextHolder.getContext().getAuthentication());//함수 계속 재사용하는거라 단순 계산에만 사용
+        Map<String, String> map = new HashMap<>();
+        map.put("token", jwt);
+        return ApiResponse.<Map<String, String>>builder().data(map).build();
     }
 
 }
