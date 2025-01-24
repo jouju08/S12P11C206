6546700b37a95c431e76dc4be05c4905955d4d44
임채현 <chi_927@naver.com>
2025-01-24 17:46:51 +0900

    [FE] chore: tailwind config 수정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
@@ -7,7 +6,37 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        NotoSans: ['Noto Sans KR', ...defaultTheme.fontFamily.sans],
+        NPSfont: ['NPSfont', ...defaultTheme.fontFamily.sans],
+        CuteFont: ['CuteFont', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        main: {
+          background: '#FFF7E1',
+          beige: '#ECCDB4',
+          btn: '#FFDEA9',
+          carrot: '#FF8462',
+          choose: '#E86161',
+          pink: '#FFEDED',
+          point: '#FFC8A4',
+          point2: '#FFBF78',
+          strawberry: '#FFB0B0',
+          success: '#A0EE6F',
+        },
+        text: {
+          first: '#222222',
+          second: '#515151',
+          third: '#B1B1B1',
+          white: '#FFFFFF',
+        },
+        gray: {
+          50: '#FFFFFF',
+          100: '#E7E7E7',
+          200: '#CFCFCF',
+          300: '#9F9F9F',
+          400: '#787878',
+          500: '#4B4B4B',
+          600: '#000000',
+        },
       },
     },
   },
