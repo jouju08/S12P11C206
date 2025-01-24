aee4e7a69a91d7f0f20a30c63d18440b521e4ddf
chaehyeon <chi_927@naver.com>
2025-01-24 16:54:42 +0900

    [FE] refactor: console.log 있던 거 삭제

--- frontend/src/components/Main/NavMenu.jsx
+++ frontend/src/components/Main/NavMenu.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function NavMenu({ location, children }) {
-  console.log(location);
-  console.log(children);
-
   const path = '/Main/';
 
   return (
