6fc1ddded7f6914fdf409efc58ccb47d6a514808
chaehyeon <chi_927@naver.com>
2025-02-02 17:00:24 +0900

    [FE] feat : tale layout에서 header 뜨도록 수정

--- frontend/src/common/layout/TaleLayout.jsx
+++ frontend/src/common/layout/TaleLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import TaleRoomHeader from '../Header/TaleRoomHeader';
 
 export default function TaleLayout() {
   return (
     <div>
+      <TaleRoomHeader />
       <Outlet />
     </div>
   );
