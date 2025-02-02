1351e43bff671246e6f738511b180ff8df67ccb1
chaehyeon <chi_927@naver.com>
2025-02-03 02:06:17 +0900

    [FE] feat: 요정 말풍선, 게임 속 참여현황 컴포넌트 생성

---
+++ frontend/src/components/Common/FairyChatBubble.jsx
@@ -0,0 +1,11 @@
+export default function FairyChatBubble({ children }) {
+  return (
+    <div
+      className="w-[287px] h-[116px] text-text-first service-bold3 text-center justify-center items-center inline-flex"
+      style={{
+        backgroundImage: "url('/Common/fairy-chat-bubble.png')",
+      }}>
+      {children}
+    </div>
+  );
+}
