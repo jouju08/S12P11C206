109df814e5ea1c7dd3bc5658dc9e570b630b9dc6
vrooming13 <vrooming13@gmail.com>
2025-02-03 17:25:31 +0900

    [FE] fix : 불필요 JSX 삭제제

--- frontend/src/utils/room.jsx
+++
@@ -1,7 +0,0 @@
-import SockJS from 'sockjs-client';
-import { Client } from '@stomp/stompjs';
-import { useTaleRoom } from '@/store/roomStore';
-
-const BASE_WS_URL = 'http://192.168.100.136:8080/ws';
-
-let stompClient = null;
