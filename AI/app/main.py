5644eef3c4fe7e41b3fc77acb4f02c943e40d58b
이영재 <joker901010@gmail.com>
2025-02-03 17:27:33 +0900

    [CD/CD] AI-Server 컨테이너 문제 수정

--- AI/app/main.py
+++ AI/app/main.py
@@ -11,8 +11,7 @@ app = FastAPI(title="MyFairy AI API")
 def hello():
     return {"hello": "world"}
 
-if __name__ == "__main__":
-    app.include_router(routers.ask_router)  # ask router 추가
-    app.include_router(routers.gen_router)  # gen router 추가
-    uvicorn.run(app, host=config.FAST_API_HOST,
-                port=config.FAST_API_PORT)
+app.include_router(routers.ask_router)  # ask router 추가
+app.include_router(routers.gen_router)  # gen router 추가
+uvicorn.run(app, host=config.FAST_API_HOST,
+            port=config.FAST_API_PORT)
