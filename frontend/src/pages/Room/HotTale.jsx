cfc2ac05fd9a0c2d48993f3024bb27f0bc25d2e1
chaehyeon <chi_927@naver.com>
2025-02-05 10:33:10 +0900

    [FE] refactor: 햇동화 읽는 부분 필요없는 주석 삭제

--- frontend/src/pages/Room/HotTale.jsx
+++ frontend/src/pages/Room/HotTale.jsx
@@ -77,27 +77,6 @@ const HotTale = () => {
         </div>
       </div>
 
-      {/* 이전 화살표 */}
-      {/* <div
-        onClick={() => setPageNum((prev) => prev - 1)}
-        className="absolute bottom-3 right-24">
-        <img
-          src="/Common/arrow-left.png"
-          alt="이전 화살표"
-          className="w-[50px] h-[50px] "
-        />
-      </div> */}
-      {/* 다음 화살표 */}
-      {/* <div
-        onClick={() => setPageNum((prev) => prev + 1)}
-        className="absolute bottom-3 right-3">
-        <img
-          src="/Common/arrow-left.png"
-          alt="다음 화살표"
-          className="w-[50px] h-[50px] -scale-x-100"
-        />
-      </div> */}
-
       <div className="absolute bottom-3 right-1/3 transform -translate-x-1/2 -translate-y-1/2">
         <TaleNavigation
           pageNum={pageNum}
@@ -109,34 +88,3 @@ const HotTale = () => {
 };
 
 export default HotTale;
-
-// const TaleNavigation = ({ pageNum, setPageNum }) => {
-//   return (
-//     <div className="flex justify-between w-[175px]">
-//       {/* 이전 화살표 */}
-//       <div
-//         onClick={() => pageNum > 0 && setPageNum((prev) => prev - 1)}
-//         className={`${pageNum <= 0 ? 'cursor-not-allowed' : 'cursor-pointer'}`}>
-//         <img
-//           src={'/Common/arrow-left.png'}
-//           alt="이전 화살표"
-//           className={`w-[50px] h-[50px] ${pageNum <= 0 ? 'opacity-50 grayscale' : ''}`}
-//         />
-//       </div>
-
-//       {/* 몇 페이지인지 */}
-//       <span className="service-accent1 text-right">{pageNum + 1}</span>
-
-//       {/* 다음 화살표 */}
-//       <div
-//         onClick={() => pageNum < 3 && setPageNum((prev) => prev + 1)}
-//         className={`${pageNum >= 3 ? 'cursor-not-allowed' : 'cursor-pointer'}`}>
-//         <img
-//           src={'/Common/arrow-left.png'}
-//           alt="다음 화살표"
-//           className={`w-[50px] h-[50px] -scale-x-100 ${pageNum >= 3 ? 'opacity-50 grayscale' : ''}`}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
