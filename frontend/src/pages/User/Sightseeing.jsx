a474aa72e38c787f3fa71c73fe6be017e21b2bd3
chaehyeon <chi_927@naver.com>
2025-02-09 23:27:22 +0900

    [FE] feat: 그림 구경 컴포넌트 배치

--- frontend/src/pages/User/Sightseeing.jsx
+++ frontend/src/pages/User/Sightseeing.jsx
@@ -1,5 +1,106 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import GalleryItem from '@/components/Common/GalleyItem';
 
 export default function Sightseeing() {
-  return <div>sightseeing</div>;
+  const [sortBy, setSortBy] = useState('전체보기');
+  return (
+    <div className="w-[1024px] h-fit px-[25px]">
+      <h1 className="text-center text-text-first service-accent1 mx-auto mb-3">
+        그림 구경
+      </h1>
+
+      {/* 지금 인기 있는 그림 */}
+      <div className='w-[974px] h-[585px] mt-[30px] relative bg-[url("/Sightseeing/award-stand.png")] bg-contain bg-center bg-no-repeat'>
+        <div className="w-[250px] h-[170px] absolute top-[23px] left-[30px] py-5 flex-col items-center gap-2.5 inline-flex overflow-hidden">
+          <div className="text-text-first service-accent2">
+            지금 인기 있는 그림!
+          </div>
+          <div className="text-text-second service-regular3">
+            나도 자랑하고 감상해봐요
+          </div>
+          <Link
+            to={'/gallery'}
+            className="px-3.5 py-2 bg-main-point2 rounded-[30px] shadow-[4px_4px_4px_0px_rgba(0,0,0,0.15)] justify-center items-center gap-2.5 text-white service-bold3 inline-flex overflow-hidden">
+            내 그림 꾸러미 가기
+          </Link>
+        </div>
+
+        {/* 3등 이름 */}
+        <div className="w-[145px] h-[89px] absolute bottom-1 left-[248px] px-3 flex-col justify-center items-center gap-px inline-flex overflow-hidden">
+          <img
+            className="w-[58px] h-[58px] relative rounded-[100px]"
+            src="/Main/profile-img.png"
+          />
+          <div className="text-center text-white service-bold3">닉네임213</div>
+        </div>
+
+        {/* 3등 이미지 */}
+        <Link to={'/'}>
+          <img
+            className="w-[145px] h-[145px] absolute origin-top-left top-[277px] left-[253px] ]"
+            src="/Sightseeing/test1.png"
+          />
+        </Link>
+
+        {/* 1등 이름 */}
+        <div className="w-[145px] h-[89px] absolute bottom-[14px] left-[411px] px-3 flex-col justify-center items-center gap-px inline-flex overflow-hidden">
+          <img
+            className="w-[58px] h-[58px] relative rounded-[100px]"
+            src="/Main/profile-img.png"
+          />
+          <div className="text-center text-white service-bold3">닉네임213</div>
+        </div>
+
+        {/* 1등 이미지 */}
+        <Link>
+          <img
+            className="w-[145px] h-[145px] absolute origin-top-left top-[216px] left-[414px] ]"
+            src="/Sightseeing/test2.png"
+          />
+        </Link>
+
+        {/* 2등 이름 */}
+        <div className="w-[145px] h-[89px] absolute bottom-1 right-[253px] px-3 flex-col justify-center items-center gap-px inline-flex overflow-hidden">
+          <img
+            className="w-[58px] h-[58px] relative rounded-[100px]"
+            src="/Main/profile-img.png"
+          />
+          <div className="text-center text-white service-bold3">닉네임213</div>
+        </div>
+
+        {/* 2등 이미지 */}
+        <Link>
+          <img
+            className="w-[145px] h-[145px] absolute origin-top-left top-[302px] right-[248px] ]"
+            src="/Sightseeing/test3.png"
+          />
+        </Link>
+      </div>
+
+      {/* 작품 갤러리 */}
+      <div className="w-[974px] h-fit px-[22px]">
+        <h1 className="service-accent2 mt-[10px] ">작품 갤러리</h1>
+        {/* 정렬 선택 */}
+        <div className="text-right">
+          <select
+            value={sortBy}
+            className="mr-4 border bg-white rounded-md service-regular2 px-4 w-[180px] h-[40px] relative z-50">
+            <option value="전체보기">전체 보기</option>
+            <option value="최신순">최신순</option>
+            <option value="과거순">인기순</option>
+          </select>
+        </div>
+
+        {/* 그림 갤러리 */}
+        <div className="grid grid-flow-row grid-cols-4 gap-4 mt-[30px]">
+          {Array(7)
+            .fill(null)
+            .map((_, idx) => (
+              <GalleryItem key={idx} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
