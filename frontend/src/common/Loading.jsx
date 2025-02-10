602f0ac1f3c45ef15c2495d53dcaf27e12e5f83b
chaehyeon <chi_927@naver.com>
2025-02-10 23:56:25 +0900

    [FE] loading, 부모 컨테이너에 따른 loading 이미지 세팅

--- frontend/src/common/Loading.jsx
+++ frontend/src/common/Loading.jsx
@@ -3,6 +3,26 @@ import React, { useRef, useEffect, useState } from 'react';
 import '@/styles/loading.css';
 
 export const Loading = () => {
+  const parentRef = useRef(null);
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    const checkHeight = () => {
+      if (parentRef.current) {
+        const height = parentRef.current.clientHeight;
+        setShowImage(height >= 320);
+      }
+    };
+
+    checkHeight(); // 초기 체크
+
+    // 리사이즈 이벤트 리스너 추가
+    window.addEventListener('resize', checkHeight);
+
+    // 클린업 함수
+    return () => window.removeEventListener('resize', checkHeight);
+  }, []);
+
   return (
     // <div
     //   className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
@@ -12,15 +32,18 @@ export const Loading = () => {
     //   </span>
     // </div>
     <div
+      ref={parentRef}
       className="w-full h-full flex flex-col gap-5 justify-center items-center"
       id="loading">
-      <div>
-        <img
-          src="/Common/run-fairy.png"
-          alt="로딩화면"
-          className="w-[200px] h-[200px] shaking-image"
-        />
-      </div>
+      {showImage && (
+        <div>
+          <img
+            src="/Common/run-fairy.png"
+            alt="로딩화면"
+            className="w-[200px] h-[200px] shaking-image"
+          />
+        </div>
+      )}
       <div className="font-NPSfont text-6xl text-text-first">
         <span>곧</span>
         <span className="w-[25px]"></span>
