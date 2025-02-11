e03cc28fa936bcec4e5ddf62a2d97240de0d9989
chaehyeon <chi_927@naver.com>
2025-02-11 15:05:18 +0900

    [FE] feat: mainoutlet에 친구목록 누르면 친구 창 뜨도록 수정

--- frontend/src/common/layout/MainLayout.jsx
+++ frontend/src/common/layout/MainLayout.jsx
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Footer from '../Footer';
 import AuthHeader from '../Header/AuthHeader';
 import DefaultHeader from '../Header/DefaultHeader';
 import { useUser } from '@/store/userStore';
+import Friends from '@/components/Friend/Friend';
 
 export default function MainLayout() {
   const { isAuthenticated } = useUser();
   const location = useLocation();
+  const [showFriend, setShowFriend] = useState(false);
 
   const isCollectionPage = location.pathname === '/collection';
 
   return (
     <div
-      className={`${isCollectionPage ? 'bg-main-beige' : 'bg-main-background'}`}>
-      {isAuthenticated ? <AuthHeader /> : <DefaultHeader />}
+      className={`relative ${isCollectionPage ? 'bg-main-beige' : 'bg-main-background'}`}>
+      {isAuthenticated ? (
+        <AuthHeader
+          showFriend={showFriend}
+          setShowFriend={setShowFriend}
+        />
+      ) : (
+        <DefaultHeader />
+      )}
 
       {/* background option */}
       {isCollectionPage ? (
@@ -25,9 +35,14 @@ export default function MainLayout() {
       ) : null}
 
       {/* 최소 높이 주는 css 삭제 */}
-      <div className="flex w-3/4 justify-center items-center mx-auto border-solid border-2 border-indigo-600">
+      <div className="relative flex w-3/4 justify-center items-center mx-auto border-solid border-2 border-indigo-600">
         <Outlet />
       </div>
+      {showFriend && (
+        <div className="absolute top-[110px] right-1/4 z-50">
+          <Friends />
+        </div>
+      )}
       <Footer />
     </div>
   );
