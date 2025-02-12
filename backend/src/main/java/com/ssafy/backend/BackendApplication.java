bbb286d3a0205c9bfe33eded0bdf2f21afbe0b25
이영재 <joker901010@gmail.com>
2025-02-12 09:34:12 +0900

    [BE] TimeZone 수정

--- backend/src/main/java/com/ssafy/backend/BackendApplication.java
+++ backend/src/main/java/com/ssafy/backend/BackendApplication.java
@@ -1,8 +1,11 @@
 package com.ssafy.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
--- frontend/src/store/galleryDetailStore.js
+++ frontend/src/store/galleryDetailStore.js
@@ -54,7 +54,7 @@ const galleryPageActions = (set, get) => ({
     try {
       const response = await api.post('/gallery/like', {
         id: galleryId,
-        hasLiked: !hasLiked,
+        hasLiked: hasLiked,
       });
       console.log('좋아요 토글에 따른 응답 ', hasLiked);
       console.log('좋아요 토글에 따른 응답 ', response);
