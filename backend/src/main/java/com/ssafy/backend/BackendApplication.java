0fc9afa3ea005515578319ae543be9ff904c08a7
이영재 <joker901010@gmail.com>
2025-02-12 09:37:31 +0900

    [BE] fix:TimeZone

--- backend/src/main/java/com/ssafy/backend/BackendApplication.java
+++ backend/src/main/java/com/ssafy/backend/BackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.LocalDateTime;
 import java.util.TimeZone;
 
 @SpringBootApplication
