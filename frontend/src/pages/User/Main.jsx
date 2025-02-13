b1dc21c28a1770cf5a0b9dbb81139d93a236981c
chaehyeon <chi_927@naver.com>
2025-02-13 15:03:53 +0900

    [FE] 메인에 동화방 없으면 없다고 뜨도록 다시 수정

--- frontend/src/pages/User/Main.jsx
+++ frontend/src/pages/User/Main.jsx
@@ -204,7 +204,7 @@ export default function Main() {
           만들어진 동화방
         </div>
         <div className="h-[300px] flex items-center text-center">
-          {taleData ? (
+          {taleData.length != 0 ? (
             <Swiper
               slidesPerView={3}
               spaceBetween={-10}
