55785f2fc4bba028bfa0c9241fd2979acc2e848a
vrooming13 <vrooming13@gmail.com>
2025-02-15 17:57:01 +0900

    fix : websocket config

--- frontend/src/store/roomStore.js
+++ frontend/src/store/roomStore.js
@@ -26,7 +26,7 @@ const roomActions = (set, get) => ({
   //소켓 연결
   connectRoom: async () => {
     return new Promise((resolve, reject) => {
-      const socket = new SockJS(import.meta.env.VITE_WS_URL_LOCAL);
+      const socket = new SockJS('/ws');
 
       console.log(socket);
       const stompClient = new Client({
