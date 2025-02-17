868e61ad74318036d93e9b95789bbc7692e981cc
chaehyeon <chi_927@naver.com>
2025-02-17 09:09:37 +0900

    [FE] fix: room.jsx 만들어진 방 없을 때 뜨는 부분 수정

--- frontend/src/components/Room/RoomBtn.jsx
+++ frontend/src/components/Room/RoomBtn.jsx
@@ -63,7 +63,7 @@ export default function RoomBtn({ isSingle, location, children }) {
   return (
     <div
       onClick={() => (isSingle ? SingleTalePlay() : multiTalePlay())}
-      className="cursor-pointer h-[45px] px-3 py-1 bg-main-btn rounded-[50px] border border-gray-200 justify-center items-center gap-1.5 inline-flex overflow-hidden hover:bg-main-point transition-all ease-linear">
+      className="cursor-pointer h-[45px] px-3 py-1 bg-main-btn rounded-[50px] border border-gray-200 justify-center items-center gap-1.5 inline-flex overflow-hidden hover:bg-main-carrot transition-all ease-linear">
       <div className="w-8 h-8 bg-white/50 rounded-[50px]">
         <img
           src={`Room/${location}`}
