204d0b1b8e16b6256d1ef9d498d58a67b6501104
이영재 <joker901010@gmail.com>
2025-02-17 15:39:19 +0900

    [FE] 설정 변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: [{ find: '@', replacement: '/src' }], // '@'를 '/src' 폴더로 대체
   },
